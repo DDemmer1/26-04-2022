1508afdcdd248210017c551f453a5145dbe752ba
DDemmer1 <Dennis.Demmer@web.de>
2022-05-03 10:09:38 +0200

    added random int

---
+++ src/de/demmer/dennis/ApplicationTwo.java
@@ -0,0 +1,13 @@
+package de.demmer.dennis;
+
+import java.util.Random;
+
+public class ApplicationTwo {
+	
+	public static void main(String[] args) {
+		
+		int i = new Random().nextInt(101);
+		
+	}
+
+}
